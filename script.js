8404ef0959059a6c5c312e7ec0c3f44fcb188f78
JeffLucker <jeferson.silvatd12@gmail.com>
2025-01-23 12:52:25 -0300

    stars

--- script.js
+++
@@ -1,7 +0,0 @@
-const spans = document.querySelectorAll('.star');
-
-// Define a variável CSS com base no atributo data-i
-spans.forEach(span => {
-  const iValue = span.getAttribute('data-i');
-  span.style.setProperty('--size', iValue); // Define a variável CSS com o valor de data-i
-});
